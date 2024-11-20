193bd2417d1dbec65f5ff6b6178ee990c6fae1bc
FlorianNeuenschwander <162428047+FlorianNeuenschwander@users.noreply.github.com>
2024-11-20 16:33:50 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Typography from "@mui/material/Typography";
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h1">hallo welt</Typography>
+    </div>
+  );
+}
+
+export default App;
